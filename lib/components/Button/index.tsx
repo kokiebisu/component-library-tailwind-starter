24bd5f4632e2b016aeb7c07b15f40049a41ae926
Kenichi Okiebisu <a01056715@gmail.com>
2021-02-13 13:00:24 -0800

    fix: styling

--- lib/components/Button/index.tsx
+++ lib/components/Button/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "../../styles.css";
 
-const Button = ({ message = "Hello World" }) => {
-  return <button className="bg-red-500 text-lg">{message}</button>;
+const Button = ({ message = "Button" }) => {
+  return (
+    <button className="p-3 bg-gradient-to-r from-blue-600 to-blue-400 rounded-md text-white">
+      {message}
+    </button>
+  );
 };
 
 export { Button };
